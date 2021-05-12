577aa4c9ecea9ec5125ccbab5d259b2b9de95ec9
mdalesio <michael.dalesio@gmail.com>
2021-05-12 17:29:32 -0400

    Choose xls* file in directory; gauss_stagger.xlsx added

--- rampManager/rampManager.py
+++ rampManager/rampManager.py
@@ -1,19 +1,27 @@
 from p4p.client.thread import Context
 import pandas as pd
-import time
+import time, os, re
 
+path = os.path.abspath(os.path.dirname(__file__))
 ctxt = Context('pva')
-infile = "gauss.xlsx"
+
 cols = [x for x in range(0,12)]
 lPVs = ["amplifier{0}".format(x) for x in range(1,11)]
 
+xlsx_files = [f for f in os.listdir(path) if re.match('.*\.xls.*', f)]
+print("Which schedule would you like to use?")
+response = None
+while response not in xlsx_files:
+        response = input("Please choose from the following {0}: ".format(xlsx_files))
+infile = path + "/" + response
 df = pd.read_excel(infile, header=1, usecols=cols)
 # print (df)
 
-for event in df["EVENT"]:
-    print("Event: {0}".format(event))
-    for pv in lPVs:
-        print("{0}: {1}".format(pv, df.loc[event][pv]))
-        ctxt.put(pv, df.loc[event][pv])
-    print("Sleep: {0}".format(df.loc[event]["TIME"]))
-    time.sleep(df.loc[event]["TIME"])
+while True:
+    for event in df.index:
+        print("Event: {0}".format(event))
+        for pv in lPVs:
+            print("{0}: {1}".format(pv, df.loc[event][pv]))
+            ctxt.put(pv, df.loc[event][pv])
+        print("Sleep: {0}".format(df.loc[event]["TIME"]))
+        time.sleep(df.loc[event]["TIME"])
