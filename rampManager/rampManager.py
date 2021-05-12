df910f037b99cb4e8cafa739b2277cf28cd60a95
mdalesio <michael.dalesio@gmail.com>
2021-05-12 16:07:42 -0400

    Input from xlsx includes time to sleep between events; Script puts to PVs after sleep

--- rampManager/rampManager.py
+++ rampManager/rampManager.py
@@ -4,23 +4,16 @@ import time
 
 ctxt = Context('pva')
 infile = "gauss.xlsx"
-cols = [x for x in range(0,11)]
+cols = [x for x in range(0,12)]
+lPVs = ["amplifier{0}".format(x) for x in range(1,11)]
 
-df = pd.read_excel(infile, header=1, index_col="EVENT", usecols=cols)
+df = pd.read_excel(infile, header=1, usecols=cols)
 # print (df)
 
-def ramp(event):
+for event in df["EVENT"]:
     print("Event: {0}".format(event))
-    for pv in df:
+    for pv in lPVs:
         print("{0}: {1}".format(pv, df.loc[event][pv]))
         ctxt.put(pv, df.loc[event][pv])
-
-sub = ctxt.monitor('eventcount', ramp)
-
-try:
-	while True:
-		time.sleep(1)
-except KeyboardInterrupt:
-	sub.close()
-	("Keyboard Interrupted")
-	pass
+    print("Sleep: {0}".format(df.loc[event]["TIME"]))
+    time.sleep(df.loc[event]["TIME"])
